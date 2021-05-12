f5802bc36da90ca3cd7bcfd34a0ae8255fa41cb0
miguel-rod <mike.rodriguez91@gmail.com>
2021-05-12 12:20:55 -0500

    first commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$fileLocation = Join-Path $toolsDir 'setup.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  file         = $fileLocation
+
+  softwareName  = 'testpkg*'
+
+  
+  checksum      = '78ECF8F9CF071DAE521D8DCCFC7EDC21226B10D722955D07431D627E46F964AF'
+  checksumType  = 'sha256'
+
+  silentArgs    = "/S"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
